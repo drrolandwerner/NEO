6731c56f0b262234b23b33eb11da80261e282e58
drrolandwerner <drrolandwerner@t-online.de>
2014-06-07 07:48:22 +0200

    NEO

--- GFKNEO.js
+++ GFKNEO.js
@@ -13,7 +13,7 @@ function loadjscssfile(filename, filetype){
  if (typeof fileref!="undefined")
   document.getElementsByTagName("head")[0].appendChild(fileref)
 }
-<script language="JavaScript">
+
 function GetHttpRequest()
 {
 if ( window.XMLHttpRequest ) // Gecko
